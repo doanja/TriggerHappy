f1fe99303129142c1aa13a8c37cf0d9a98b652c4
John Doan <doanja@plu.edu>
2016-05-04 10:59:22 -0700

    Archer Elizabeth
    New:
    - Archer Elizabeth enemy prefab and animations
    - Elizabeth's arrow & particles

--- Android Game/Assets/Code/EnemyAI Scripts/PatrolRaycastChargeAI.cs
+++ Android Game/Assets/Code/EnemyAI Scripts/PatrolRaycastChargeAI.cs
@@ -1,26 +1,21 @@
 ﻿using UnityEngine;
 
-/* 
-* Resource: https://www.youtube.com/watch?v=re6fookKraU&index=28&list=PLt_Y3Hw1v3QSFdh-evJbfkxCK_bjUD37n 
-* 
-* This class handles how a generic enemy AI preforms. This class implements an instance of the
-* CharacterController2D, which is used to check collisions, and the speed/velocity of this GameObject.
-* This GameObject will also be able to shoot projectiles at the player. The player is rewarded points 
-* for killing this GameObject, and can be awarded poitns form killing this GameObject's projectiles.
+/*
+* Resource: Adapted from PatrolRaycastShootAI
+*
+* This class will move the GameObject back and forth between two platforms, changing
+* direction upon collision with the platform. This GameObject will double its
+* MovementSpeed if raycast detected the Player.
 */
-public class PatrolRaycastShootAI : MonoBehaviour, ITakeDamage, IPlayerRespawnListener {
+public class PatrolRaycastChargeAI : MonoBehaviour, ITakeDamage, IPlayerRespawnListener
+{
 
     // Parameters   
-    public float Speed;                 // travel speed of this GameObject
-    public float FireRate = 1;          // cooldown time after firing a projectile
-    private float Cooldown;             // the amount of time this GameObject can shoot projectiles
-    public Projectile Projectile;       // this GameObject's projectile
+    public float MovementSpeed;         // travel speed of this GameObject   
     public GameObject DestroyedEffect;  // the destroyed effect of this GameObject   
-    public Transform ProjectileFireLocation;    // the location of which the projectile is fired at
     public int PointsToGivePlayer;      // points awarded to the player upon killing this GameObject
 
-    // Sound
-    public AudioClip ShootSound;            // the sound when this GameObject shoots a projectile
+    // Sound   
     public AudioClip EnemyDestroySound;     // sound played when this GameObject is destroyed    
 
     // Character Essentials
@@ -33,44 +28,36 @@ public class PatrolRaycastShootAI : MonoBehaviour, ITakeDamage, IPlayerRespawnLi
     public int Health { get; private set; }     // this GameObject's current health    
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         _controller = GetComponent<CharacterController2D>();
         _direction = new Vector2(-1, 0);    // this GameObject will move the left upon initialization
         _startPosition = transform.position;
         Health = MaxHealth;
     }
-	
-	// Update is called once per frame
-	public void Update () {
 
+    // Update is called once per frame
+    public void Update()
+    {
         // Sets the x-velocity of this GameObject
-        _controller.SetHorizontalForce(_direction.x * Speed);
+        _controller.SetHorizontalForce(_direction.x * MovementSpeed);
 
         // Checks to see if this GameObject is colliding with something in the same direction
         if ((_direction.x < 0 && _controller.State.IsCollidingLeft) || (_direction.x > 0 && _controller.State.IsCollidingRight))
         {
             _direction = -_direction; // switches direction
             transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-        }
-
-        // Handles when this GameObject cannot shoot
-        if ((Cooldown -= Time.deltaTime) > 0)
-            return;       
+        }       
 
         // Casts rays to detect player
         var raycast = Physics2D.Raycast(transform.position, _direction, 10, 1 << LayerMask.NameToLayer("Player"));
         if (!raycast)
             return;
-
-        // Instantiates the projectile, and initilializes the speed, and direction of the projectile
-        var projectile = (Projectile)Instantiate(Projectile, ProjectileFireLocation.position, ProjectileFireLocation.rotation);
-        projectile.Initialize(gameObject, _direction, _controller.Velocity);
-        Cooldown = FireRate; // time frame, when projectiles can be shot from this GameObject
-
-        // Handles Sound
-        if(ShootSound != null)
-            AudioSource.PlayClipAtPoint(ShootSound, transform.position);
-	}
+        else 
+        {
+            MovementSpeed = MovementSpeed * 2; // raycast returned true, double this GameObject's MovementSpeed
+        }              
+    }
 
     /*
    * @param damage, the damage this GameObject receives
@@ -114,7 +101,7 @@ public class PatrolRaycastShootAI : MonoBehaviour, ITakeDamage, IPlayerRespawnLi
     {
         // Re-initializes this GameObject's direction, and start position
         _direction = new Vector2(-1, 0);
-        transform.localScale = new Vector3(1, 1, 1);       
+        transform.localScale = new Vector3(1, 1, 1);
         gameObject.SetActive(true);                     // shows this GameObject       
 
         // Resets health

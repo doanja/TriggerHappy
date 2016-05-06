237d39875cc4b29be8ba8a52c4377e8c4bfb803b
John Doan <doanja@plu.edu>
2016-05-05 18:24:04 -0700

    Minor Updates & Level_1_C
    New:
    - New pickup prefabs with animations
    - IceBlock 4x4 platform
    - Level_1_C Map with enemy (still buggy)
    
    Updates:
    - Elizabeth that spawns from portals now have a lifetime
    
    Bugs:
    - no limit on spawns

---
+++ Android Game/Assets/Code/EnemyAI Scripts/TimeToLive.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class TimeToLive : MonoBehaviour {
+
+    public float lifetime;            // the amount of time this GameObject lives    
+    public GameObject BlowupEffect;     // the blowup effect
+    public AudioClip BlowupSound;     // sound played when this GameObject collides with the Player
+
+    // Update is called once per frame
+    void Update () {
+        // The amount of time this projectile lives
+        if ((lifetime -= Time.deltaTime) <= 0)
+        {
+            AudioSource.PlayClipAtPoint(BlowupSound, transform.position);
+            Instantiate(BlowupEffect, transform.position, transform.rotation);
+            Destroy(this.gameObject);
+            return;
+        }
+    }
+}
